645a63ab897e08cc7182c3d589be97480ccdb70c
unknown <zhangxudong_sun@sina.com>
2018-06-05 21:09:55 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor(private $http: HttpClient) {
+    // this.http.get("/products").map((res) => res.json());
+    this.$http.get("/article").subscribe(data => {
+      console.log(data);
+    });
+  }
 }
