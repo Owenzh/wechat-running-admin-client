f9adb6358269733bc883381e85d84c7d4b927006
owenzh <zhangxudong_sun@sina.com>
2018-06-13 18:32:12 +0800

    active nav for header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,12 +8,19 @@ import { Location } from '@angular/common';
 export class HeaderComponent implements OnInit {
 
   constructor(private $location: Location) { }
-
+  RouterMap = {
+    home: '',
+    post: '/article_post',
+    list: '/article_list',
+    report: '/article_report'
+  };
   ngOnInit() {
   }
   public setNavClass(hash: string): string {
-    // let current_path: string = this.$location.path();
-    // if(current_path.includes)
-    return 'am-active';
+    const current_path: string = this.$location.path();
+    if (current_path === this.RouterMap[hash]) {
+      return 'am-active';
+    }
+    return '';
   }
 }
