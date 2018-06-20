66900937699f5e8a2a5e459974103e045a307578
owenzh <zhangxudong_sun@sina.com>
2018-06-20 15:32:41 +0800

    create article form.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ArticlePostComponent } from './article-post/article-post.component';
 import { ArticleListComponent } from './article-list/article-list.component';
 import { ArticleReportComponent } from './article-report/article-report.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { QuillModule } from 'ngx-quill';
 
 const routes: Routes = [
@@ -17,7 +18,7 @@ const routes: Routes = [
     ArticleListComponent,
     ArticleReportComponent
   ],
-  imports: [RouterModule.forRoot(routes), QuillModule, FormsModule],
+  imports: [CommonModule, RouterModule.forRoot(routes), QuillModule, FormsModule, ReactiveFormsModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/article-post/article-post.component.ts
+++ src/app/article-post/article-post.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-article-post',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article-post.component.css']
 })
 export class ArticlePostComponent implements OnInit {
-  editorContent = '';
+  articleForm: FormGroup;
   constructor() { }
-
   ngOnInit() {
+    this.articleForm = new FormGroup({
+      article_title: new FormControl('', [Validators.required]),
+      article_type: new FormControl('basic', [Validators.required]),
+      article_content: new FormControl('文章内容', [Validators.required])
+    });
   }
-
+  get article_title() { return this.articleForm.get('article_title'); }
+  onSubmit() {
+    console.log(this.article_title);
+    console.log(this.articleForm.value);
+  }
 }
